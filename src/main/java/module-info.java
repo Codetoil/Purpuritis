551835697c35b796807924950c69a0b1f5e00ab6
Angelina Michalek <ianthisawesomee@gmail.com>
2025-09-24 19:22:48 -0400

    Removing module-info.java, datafixer library is not a module.

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module io.codetoil.purpuritis {
-    requires com.google.common;
-    requires datafixerupper;
-    requires net.minecraftforge.forge;
-    requires net.minecraftforge.javafmlmod;
-    requires org.apache.logging.log4j;
-    requires org.jetbrains.annotations;
-    requires net.minecraftforge.fmlcore;
-}
