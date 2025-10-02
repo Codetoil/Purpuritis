3147887f10c32a3e603eb4601bfccfee86210fe0
Angelina Michalek <ianthisawesomee@gmail.com>
2025-10-01 21:36:09 -0400

    Progress! (Back to problem with private classes (transformer time!))

--- src/main/java/io/codetoil/purpuritis/world/item/PurpuredItemHelper.java
+++ src/main/java/io/codetoil/purpuritis/world/item/PurpuredItemHelper.java
@@ -17,26 +17,24 @@ import java.lang.reflect.InvocationTargetException;
 public class PurpuredItemHelper {
 
     public static final DynamicRegistriesObjectHelper<Item> ITEM_DYNAMIC_REGISTRIES_OBJECT_HELPER =
-            new DynamicRegistriesObjectHelper<>((Constructor<? extends Item> constructor) -> {
-                for (int index = 0; index < constructor.getParameterCount(); index++) {
-                    if (constructor.getParameterTypes()[index] == Block.class) {
-                        return (Item item) -> {
-                            if (item instanceof BlockItem) {
-                                return Purpuritis.purpuredBlocks.get(((BlockItem) item).getBlock());
-                            } else if (item instanceof AirItem) {
-                                return Blocks.AIR;
-                            } else {
-                                return null;
-                            }
-                        };
-                    }
-                    if (constructor.getParameterTypes()[index] == Item.Properties.class) {
-                        return (Item item) -> {
-                            ResourceKey<Item> key = ResourceKey.create(ForgeRegistries.ITEMS.getRegistryKey(),
-                                    ForgeRegistries.ITEMS.getKey(item));
-                            return new Item.Properties().setId(key); // TODO implement custom Item properties
-                        };
-                    }
+            new DynamicRegistriesObjectHelper<>((Constructor<? extends Item> constructor, Integer index) -> {
+                if (constructor.getParameterTypes()[index] == Block.class) {
+                    return (Item item) -> {
+                        if (item instanceof BlockItem) {
+                            return Purpuritis.purpuredBlocks.get(((BlockItem) item).getBlock());
+                        } else if (item instanceof AirItem) {
+                            return Blocks.AIR;
+                        } else {
+                            return null;
+                        }
+                    };
+                }
+                if (constructor.getParameterTypes()[index] == Item.Properties.class) {
+                    return (Item item) -> {
+                        ResourceKey<Item> key = ResourceKey.create(ForgeRegistries.ITEMS.getRegistryKey(),
+                                ForgeRegistries.ITEMS.getKey(item));
+                        return new Item.Properties().setId(key); // TODO implement custom Item properties
+                    };
                 }
                 return null;
             }, ResourceLocation.fromNamespaceAndPath(Purpuritis.MOD_ID, "item"));
@@ -45,7 +43,8 @@ public class PurpuredItemHelper {
         Class<I> originalItemClass = (Class<I>) originalItem.getClass();
         try {
             return ITEM_DYNAMIC_REGISTRIES_OBJECT_HELPER.getClass(originalItemClass)
-                    .getConstructor(originalItemClass).newInstance(originalItem);
+                    .getConstructor(originalItemClass, DynamicRegistriesObjectHelper.class)
+                    .newInstance(originalItem, ITEM_DYNAMIC_REGISTRIES_OBJECT_HELPER);
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
                  NoSuchMethodException e) {
             throw new RuntimeException(e);
